dc2367de3a6862b0d221c992518d47b0c028a1da
vmcvastry <009valerio@gmail.com>
2024-07-25 21:38:16 +0200

    neo dataset

---
+++ setup_neo.sh
@@ -0,0 +1,4 @@
+mkdir neo_db_data_docker/transactions
+rm -r neo_db_data_docker/transactions/neo4j
+rm -r neo_db_data_docker/transactions/system
+tar -xzvf neo_db_data_docker/transactions.tar.gz -C neo_db_data_docker
